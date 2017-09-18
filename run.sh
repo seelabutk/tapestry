848b04988706372f5c3adc6b3fd615a5ba55699f
Mohammad Raji <moa@dragonfli.es>
2017-09-18 16:45:57 -0500

    Removed sudo from run.sh, updated sample index.html page in enchiladas, removed a debug message in PBNJ.

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ fi
 configs_dir=$1
 data_dir=$2
 
-sudo docker service create --replicas 1 --name tapestry -p 8080:9010/tcp --mount type=bind,src=$configs_dir,dst=/config --mount type=bind,src=$data_dir,dst=/mnt/seenas1/data tapestry_tapestry 
+docker service create --replicas 1 --name tapestry -p 8080:9010/tcp --mount type=bind,src=$configs_dir,dst=/config --mount type=bind,src=$data_dir,dst=/mnt/seenas1/data tapestry_tapestry 
 
