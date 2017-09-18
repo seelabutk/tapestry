6ca44d0c4dc505e2acf70a6dac1072166195c325
Mohammad Raji <moa@dragonfli.es>
2017-09-18 17:51:15 -0500

    added optional port parameter to run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 if [ $# -lt 2 ]; then
-    echo "Usage: ./run.sh <configs dir> <data dir>"
+    echo "Usage: ./run.sh <configs dir> <data dir> [port]"
     exit 2
 fi
 
 configs_dir=$1
 data_dir=$2
 
-docker service create --replicas 1 --name tapestry -p 8080:9010/tcp --mount type=bind,src=$configs_dir,dst=/config --mount type=bind,src=$data_dir,dst=/mnt/seenas1/data tapestry_tapestry 
+if [ $# -eq 3 ]; then
+    port=$3
+else
+    port=8080
+fi
+
+docker service create --replicas 1 --name tapestry -p $port:9010/tcp --mount type=bind,src=$configs_dir,dst=/config --mount type=bind,src=$data_dir,dst=/mnt/seenas1/data tapestry_tapestry 
 
