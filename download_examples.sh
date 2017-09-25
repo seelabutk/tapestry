20f6bc7167cd26a08e7420303e2d6b468d28277c
Alok Hota <ahota@vols.utk.edu>
2017-09-25 10:38:39 -0500

    added download script

---
+++ download_examples.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+tarurl="http://seelab.eecs.utk.edu/tapestry/tapestry_sample_dirs.tar.gz"
+tarfile=$(basename $tarurl)
+
+if hash axel 2>/dev/null; then
+    axel -a -n 4 $tarurl
+else
+    wget $tarurl
+fi
+
+tar xf $tarfile
