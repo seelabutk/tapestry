08fdf569f86fb9f189c0b23b232d2adc77541f2e
Alok Hota <ahota@vols.utk.edu>
2017-09-25 11:05:06 -0500

    changed URL in download script

--- download_example.sh
+++ download_example.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-tarurl="http://seelab.eecs.utk.edu/tapestry/tapestry_sample_dirs.tar.gz"
+tarurl="http://seelab.eecs.utk.edu/tapestry/tapestry_example_dirs.tar.gz"
 tarfile=$(basename $tarurl)
 
 if hash axel 2>/dev/null; then
