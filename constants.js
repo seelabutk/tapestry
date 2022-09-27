ef7878c4f7bc0a661ef23cddff7b36baad4f17f8
Hunter Price <hunterprice4044@gmail.com>
2022-09-26 20:52:36 -0400

    changed aws endpoint to current demo server

--- constants.js
+++ constants.js
@@ -4,12 +4,16 @@ function _TOGGLE() {
 	window.USE_AWS = !window.USE_AWS;
 	$('.hyperimage').each(function(i, d) {
 		$(d).data('tapestry').settings.host = _TAPESTRY_HOST();
+		$(d).data('tapestry').setup_tiles();
+		$(d).mousedown()
+		$(d).mouseup()
 	});
+
 }
 
 function _TAPESTRY_HOST() {
 	return window.USE_AWS
-		? 'http://tapestry-load-balancer-6258f8b58ebec6df.elb.us-east-2.amazonaws.com:9011'
+		? 'http://3.86.88.218/'
 		: 'https://accona.eecs.utk.edu';
 }
 
@@ -18,7 +22,9 @@ window.TAPESTRY_HOST = _TAPESTRY_HOST();
 document.addEventListener('DOMContentLoaded', function() {
 	const extra = ('' + window.location).includes('/extra/') ;
 	const index = ('' + window.location).includes('index.html');
-	if (extra || index) {
+	const home = (('' + window.location) === 'https://seelabutk.github.io/tapestry/')
+
+	if (extra || index || home) {
 		const div = document.createElement('div');
 		div.style.position = 'fixed';
 		if (extra) div.style.top = 0;
